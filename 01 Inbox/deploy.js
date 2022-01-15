045ada8a61dc83f3a86a38d7c82e634e104c5849
Yash  Jain <yash@Yashs-MacBook-Air.local>
2022-01-15 18:55:06 +0530

    added comments

--- 01 Inbox/deploy.js
+++ 01 Inbox/deploy.js
@@ -2,16 +2,22 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { abi, evm } = require('./compile');
 
+// * Truffle connects to Infura API
+// * Infura API connects to Infura Node
+// * Which is part of eth network
+// * You can also setup an eth node on local and connect to it and skip infura
+// * But for starters it is just easier
 const provider = new HDWalletProvider(
     "art void coffee frown review camera copper cycle common narrow embody favorite",
     "https://rinkeby.infura.io/v3/ef74396e201d4321a5e51007162699e9"
 );
 const web3 = new Web3(provider);
 
+// only reason why we created deploy function because we want to use async await
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-    console.log(`Deploying form ${accounts[0]}`);
 
+    console.log(`Deploying form ${accounts[0]}`);
     const result = await new web3.eth.Contract(abi)
         .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
         .send({ from: accounts[0], gas: '1000000' });
@@ -19,3 +25,7 @@ const deploy = async () => {
     console.log(`Contract deployed to ${result.options.address}`);
 };
 deploy();
+
+// Example deployment
+// Deploying form 0x60F548A0da1D0B5fE5317CbC26d566088bd90dC4
+// Contract deployed to 0x31B5dd331045e7d5a11F7c6F5F80baaE60371f29
--- 01 Inbox/test/Inbox.test.js
+++ 01 Inbox/test/Inbox.test.js
@@ -3,16 +3,21 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const { abi, evm } = require('../compile');
 
+// Connection layer between web3 and eth network
+// Here ganache provides provider for local eth network
 const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
 
+// Deploy the contract to the local eth network
 beforeEach(async () => {
     // get unlocked accounts
     accounts = await web3.eth.getAccounts();
 
-    // deploy contract
+    // * Since we are deploying the contract we need bytecode
+    // * If we only needed to interact with deployed contract,
+    // we need address of deployed contract
     inbox = await new web3.eth.Contract(abi)
         .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
         .send({ from: accounts[0], gas: '1000000' });
@@ -29,6 +34,8 @@ describe('Inbox', () => {
     });
 
     it('can change the message', async() => {
+        // transaction (method call) returns transaction hash,
+        // we usually don't assign it to anything
         await inbox.methods.setMessage('New String!!')
             .send({ from: accounts[0], gas: '1000000' });
 
