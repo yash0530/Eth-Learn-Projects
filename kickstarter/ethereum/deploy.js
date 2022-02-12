abc2c943a6b6235af9763187f72655aa63c5b90d
Yash Jain <yashjain0530@gmail.com>
2022-02-12 23:05:01 +0530

    deployed kickstarter factory

---
+++ kickstarter/ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+    "art void coffee frown review camera copper cycle common narrow embody favorite",
+    "https://rinkeby.infura.io/v3/ef74396e201d4321a5e51007162699e9"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(`Deploying form ${accounts[0]}`);
+
+    const result = await new web3.eth.Contract(compiledFactory.abi)
+        .deploy({ data: compiledFactory.evm.bytecode.object })
+        .send({ from: accounts[0], gas: 5000000 });
+
+    console.log(`Contract deployed to ${result.options.address}`);
+};
+deploy();
+
+// Deployed Contract
+// Deploying form 0x60F548A0da1D0B5fE5317CbC26d566088bd90dC4
+// Contract deployed to 0xA8903CbCCa7ba95F2A110a172FE35d7fbD864DBf
