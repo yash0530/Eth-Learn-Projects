4e7356ac55eb67605c2f1bc34f8a1c8898ebd449
Yash Jain <yashjain0530@gmail.com>
2022-03-20 00:38:24 +0530

    fixed getEntranceFee error, I am idiot

--- 06 FreeCodeCamp/BrownieFundMe/contracts/FundMe.sol
+++ 06 FreeCodeCamp/BrownieFundMe/contracts/FundMe.sol
@@ -58,7 +58,7 @@ contract FundMe {
     function getEntranceFee() public view returns (uint256) {
         uint256 minimumUSD = 50 * 10**18;
         uint256 price = getPrice();
-        uint256 precision = 1 * 1**18;
+        uint256 precision = 10**18;
         return (minimumUSD * precision) / price;
     }
 
