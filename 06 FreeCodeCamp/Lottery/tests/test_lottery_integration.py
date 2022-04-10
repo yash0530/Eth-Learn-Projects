c4e9f0ce63c51b45999deb7e2239509064b19bb5
Yash Jain <yashjain0530@gmail.com>
2022-04-10 17:30:32 +0530

    update'

---
+++ 06 FreeCodeCamp/Lottery/tests/test_lottery_integration.py
@@ -0,0 +1,27 @@
+import pytest
+import time
+from brownie import network
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+)
+from scripts.deploy_lottery import deploy_lottery
+
+
+def test_can_pick_winner():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee() * 1.0001})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee() * 1.0001})
+
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+
+    time.sleep(300)
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
